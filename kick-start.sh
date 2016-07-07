46ae6882ab4c1c9f47d6e6307457832d64e7fc81
Loc Nguyen <me@locnh.com>
2016-07-07 23:23:10 +0700

    Fix kick-start.sh

--- kick-start.sh
+++ kick-start.sh
@@ -4,6 +4,6 @@ if [[ -n $XDEBUG_HOST ]]; then
     sed -i.bak s/^zend_extension/\;zend_extension/g /etc/php.d/15-xdebug.ini
 else
     sed -i.bak s/^\;zend_extension/zend_extension/g /etc/php.d/15-xdebug.ini
-    echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/xdebug.ini
-    echo "xdebug.remote_host=$XDEBUG_HOST" >> /usr/local/etc/php/conf.d/xdebug.ini
+    echo "xdebug.remote_enable=on" >> /etc/php.d/15-xdebug.ini
+    echo "xdebug.remote_host=$XDEBUG_HOST" >> /etc/php.d/15-xdebug.ini
 fi
